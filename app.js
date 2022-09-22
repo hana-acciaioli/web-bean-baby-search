6deb73c583841e01a35faf71731e038f2e707dd4
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-22 11:04:20 -0700

    Display list of 100 Beanies/ missing zodiac sign and css needs work

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
 /* Imports */
-import { getBeanBabies } from './fetch-utils.js';
+import { getBeanieBabies } from './fetch-utils.js';
+import { renderBeanieBaby } from './render-babies.js';
 
 /* Get DOM Elements */
 
+const beanieBabyList = document.getElementById('beanie-baby-list');
+const notificationDisplay = document.getElementById('notification-display');
 /* State */
+let error = null;
+// let astroSigns = [];
+let beanieBabies = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    findBeanies();
+});
+
+async function findBeanies() {
+    const response = await getBeanieBabies();
+
+    error = response.error;
+    beanieBabies = response.data;
+
+    displayNotifications();
+    if (!error) {
+        displayBeanies();
+    }
+}
 
 /* Display Functions */
+
+function displayBeanies() {
+    beanieBabyList.innerHTML = '';
+
+    for (const beanieBaby of beanieBabies) {
+        const beanieBabyEl = renderBeanieBaby(beanieBaby);
+        beanieBabyList.append(beanieBabyEl);
+    }
+}
+
+function displayNotifications() {
+    if (error) {
+        notificationDisplay.classList.add('error');
+        notificationDisplay.textContent = error.message;
+    } else {
+        notificationDisplay.classList.remove('error');
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
--- fetch-utils.js
+++ fetch-utils.js
@@ -4,8 +4,8 @@ const SUPABASE_KEY =
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function getBeanBabies(name, type) {
-    let query = client.from('beanBabies').select('*').order('name').limit(100);
+export async function getBeanieBabies(name, type) {
+    let query = client.from('beanie_babies').select('*').order('title').limit(100);
     // eslint-disable-next-line no-empty
     if (name) {
     }
